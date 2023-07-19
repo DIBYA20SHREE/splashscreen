ddf437cf791c7aa2cf48fdefef72a2c1a7044878
Vumi <diby@shreechakravarty>
2023-07-19 09:47:48 +0530

    splash screen done

---
+++ app/src/main/java/com/example/final01/Scr2.kt
@@ -0,0 +1,11 @@
+package com.example.final01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scr2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scr2)
+    }
+}
